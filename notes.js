7fcc0fcabe1eaa6c7e99c9550c346871bb571f3e
JoseBernal06 <jose.bernal@epn.edu.ec>
2024-10-23 20:42:08 -0500

    primera version

---
+++ notes.js
@@ -0,0 +1,15 @@
+
+//! Sprint 1
+// Actividades que se van a realizar
+/*
+//* Creacion de endpoints para listar tours
+//* Creacion de endpoints para eliminar tours
+//* Creacion de endpoints para actualizar tours
+//* Creacion de endpoints para crear tours
+*/
+
+// SQL -> Tables -> registros
+// NoSQL -> Colecciones -> documentos
+
+// pasos
+//TODO: modelo - contolador - rutas - *servidor (se realiza una sola vez)
